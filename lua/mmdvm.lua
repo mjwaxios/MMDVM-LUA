949dda746d57a056d1cd54b159876d13412fac66
Michael <wyrick@umbrasil.com>
2017-04-17 18:16:02 -0400

    Added Status

--- lua/mmdvm.lua
+++ lua/mmdvm.lua
@@ -56,40 +56,47 @@ local DATATYPE = { [0x0] = "PI Header",
 		[0x0E] = "Reserved",
 		[0x0F] = "Reserved"}
 
-local f_fstart = ProtoField.uint8("mmdvm.fstart", "FrameStart", base.HEX)
-local f_len = ProtoField.uint8("mmdvm.len", "Length", base.DEC)
-local f_Command = ProtoField.uint8("mmdvm.Command", "Command", base.HEX, COMMAND)
-local f_Slot = ProtoField.uint8("mmdvm.Slot", "Slot", base.DEC, nil, 0x80)
-local f_dsync = ProtoField.uint8("mmdvm.dsync", "DSync", base.DEC, nil, 0x40)
-local f_async = ProtoField.uint8("mmdvm.async", "ASync", base.DEC, nil, 0x20)
-local f_dtype = ProtoField.uint8("mmdvm.dtype", "DType", base.HEX, DATATYPE, 0x0F)
-local f_seq = ProtoField.uint8("mmdvm.seq", "Seq", base.HEX, nil, 0x0F)
-local f_data = ProtoField.string("mmdvm.data", "Data", FT_STRING)
-local f_rxf = ProtoField.uint32("mmdvm.rxf", "RX Freq", base.DEC)
-local f_txf = ProtoField.uint32("mmdvm.txf", "TX Freq", base.DEC)
+local MODE = {
+        [00] = "IDLE",
+        [01] = "IDLE",
+        [02] = "IDLE",
+        [03] = "IDLE",
+        [04] = "IDLE",
+        [98] = "CW",
+        [99] = "LOCKOUT",
+        [100] = "ERROR",        
+}
+        
+p_mmdvm.fields.f_fstart = ProtoField.uint8("mmdvm.fstart", "FrameStart", base.HEX)
+p_mmdvm.fields.f_len = ProtoField.uint8("mmdvm.len", "Length", base.DEC)
+p_mmdvm.fields.f_Command = ProtoField.uint8("mmdvm.Command", "Command", base.HEX, COMMAND)
+p_mmdvm.fields.f_Slot = ProtoField.uint8("mmdvm.Slot", "Slot", base.DEC, nil, 0x80)
+p_mmdvm.fields.f_dsync = ProtoField.uint8("mmdvm.dsync", "DSync", base.DEC, nil, 0x40)
+p_mmdvm.fields.f_async = ProtoField.uint8("mmdvm.async", "ASync", base.DEC, nil, 0x20)
+p_mmdvm.fields.f_dtype = ProtoField.uint8("mmdvm.dtype", "DType", base.HEX, DATATYPE, 0x0F)
+p_mmdvm.fields.f_seq = ProtoField.uint8("mmdvm.seq", "Seq", base.HEX, nil, 0x0F)
+p_mmdvm.fields.f_data = ProtoField.string("mmdvm.data", "Data", FT_STRING)
+p_mmdvm.fields.f_rxf = ProtoField.uint32("mmdvm.rxf", "RX Freq", base.DEC)
+p_mmdvm.fields.f_txf = ProtoField.uint32("mmdvm.txf", "TX Freq", base.DEC)
 
 -- Generic MMDVM Commands and Responses
-local f_Cmd = ProtoField.uint8("mmdvm.Cmd", "CMD", base.HEX, COMMAND)
-local f_Reaspm = ProtoField.uint8("mmdvm.Reason", "Reason", base.HEX)
-local f_pversion = ProtoField.uint8("mmdvm.pversion", "Protocol Version", base.HEX)
-local f_verstr = ProtoField.string("mmdvm.verstr", "Hardware Version", FT_STRING)
+p_mmdvm.fields.f_Cmd = ProtoField.uint8("mmdvm.Cmd", "CMD", base.HEX, COMMAND)
+p_mmdvm.fields.f_Reaspm = ProtoField.uint8("mmdvm.Reason", "Reason", base.HEX)
+p_mmdvm.fields.f_pversion = ProtoField.uint8("mmdvm.pversion", "Protocol Version", base.HEX)
+p_mmdvm.fields.f_verstr = ProtoField.string("mmdvm.verstr", "Hardware Version", FT_STRING)
 
-local f_flags = ProtoField.uint16("mmdvm.flags", "Config Flags", base.HEX)
-local f_rxi = ProtoField.uint16("mmdvm.rxi", "RX Invert", base.HEX, nil, 0x0100)
-local f_txi = ProtoField.uint16("mmdvm.txi", "TX Invert", base.HEX, nil, 0x0200)
-local f_ptti = ProtoField.uint16("mmdvm.ptti", "PTT Invert", base.HEX, nil, 0x0400)
-local f_ysfLoDev = ProtoField.uint16("mmdvm.ysfLoDev", "YSF Low Dev", base.HEX, nil, 0x0800)
-local f_duplex = ProtoField.uint16("mmdvm.duplex", "Duplex", base.HEX, nil, 0x8000)
-local f_dstaren= ProtoField.uint16("mmdvm.dstaren", "DStar Enable", base.HEX, nil, 0x0001)
-local f_dmren= ProtoField.uint16("mmdvm.dmren", "DMR Enable", base.HEX, nil, 0x0002)
-local f_ysfen= ProtoField.uint16("mmdvm.ysfen", "DStar Enable", base.HEX, nil, 0x0004)
-local f_p25en= ProtoField.uint16("mmdvm.p25en", "P25 Enable", base.HEX, nil, 0x0008)
-  
-p_mmdvm.fields = {f_fstart, f_len, f_Command, f_Slot, f_dsync, f_async, f_dtype, f_dmrdata, f_seq, 
-  f_Cmd, f_Reason, f_pversion, f_verstr, f_rxf, f_txf,
-  f_flags, f_rxi, f_txi, f_ptti, f_ysfLoDev, f_duplex, f_dstaren, f_dmren, f_ysfen, f_p25en
-  }
+p_mmdvm.fields.f_flags = ProtoField.uint16("mmdvm.flags", "Config Flags", base.HEX)
+p_mmdvm.fields.f_rxi = ProtoField.uint16("mmdvm.rxi", "RX Invert", base.HEX, nil, 0x0100)
+p_mmdvm.fields.f_txi = ProtoField.uint16("mmdvm.txi", "TX Invert", base.HEX, nil, 0x0200)
+p_mmdvm.fields.f_ptti = ProtoField.uint16("mmdvm.ptti", "PTT Invert", base.HEX, nil, 0x0400)
+p_mmdvm.fields.f_ysfLoDev = ProtoField.uint16("mmdvm.ysfLoDev", "YSF Low Dev", base.HEX, nil, 0x0800)
+p_mmdvm.fields.f_duplex = ProtoField.uint16("mmdvm.duplex", "Duplex", base.HEX, nil, 0x8000)
 
+p_mmdvm.fields.f_dstaren= ProtoField.uint16("mmdvm.dstaren", "DStar Enable", base.HEX, nil, 0x0001)
+p_mmdvm.fields.f_dmren= ProtoField.uint16("mmdvm.dmren", "DMR Enable", base.HEX, nil, 0x0002)
+p_mmdvm.fields.f_ysfen= ProtoField.uint16("mmdvm.ysfen", "YSF Enable", base.HEX, nil, 0x0004)
+p_mmdvm.fields.f_p25en= ProtoField.uint16("mmdvm.p25en", "P25 Enable", base.HEX, nil, 0x0008)
+ 
 local fstart_Field = Field.new("mmdvm.fstart")
 local Command_Field = Field.new("mmdvm.Command")
 local dsync_Field = Field.new("mmdvm.dsync")
@@ -109,7 +116,7 @@ function p_mmdvm.dissector (buf, pinfo, root)
   local RXTX = buf(0,1):uint() == 0xE1 
 
   subtree = root:add(p_mmdvm, buf(0))
-  subtree:add(f_fstart, buf(0,1)) 
+  subtree:add(p_mmdvm.fields.f_fstart, buf(0,1)) 
   
   pinfo.cols.protocol:append(p_mmdvm.name)
   if RXTX then
@@ -118,65 +125,98 @@ function p_mmdvm.dissector (buf, pinfo, root)
     pinfo.cols.protocol:append(" RX ")  
   end
 
-  subtree:add(f_len, buf(1,1))
+  subtree:add(p_mmdvm.fields.f_len, buf(1,1))
   local Length = len_Field().value;
   
-  subtree:add(f_Command, buf(2,1)) 
+  subtree:add(p_mmdvm.fields.f_Command, buf(2,1)) 
   local Command = Command_Field().value
   
   -- ACK
   if Command == 0x70 then
-    subtree:add(f_Cmd, buf(3,1))   
+    subtree:add(p_mmdvm.fields.f_Cmd, buf(3,1))   
     pinfo.cols.info:append("ACK Command " .. buf(3,1) )
   elseif Command == 0x7F then
-    subtree:add(f_Cmd, buf(3,1))   
-    subtree:add(f_Reason, buf(4,1))   
+    subtree:add(p_mmdvm.fields.f_Cmd, buf(3,1))   
+    subtree:add(p_mmdvm.fields.f_Reason, buf(4,1))   
     pinfo.cols.info:append("NAK Command " .. buf(3,1) .. " Reason " .. buf(4,1) )
   elseif Command == 0x00 then
     pinfo.cols.info:append("Version ")
     if Length >= 4 then
-      subtree:add(f_pversion, buf(3,1))     
-      subtree:add(f_verstr, buf(4, Length - 4))
+      subtree:add(p_mmdvm.fields.f_pversion, buf(3,1))     
+      subtree:add(p_mmdvm.fields.f_verstr, buf(4, Length - 4))
     end
   elseif Command == 0x01 then
     pinfo.cols.info:append("Status ")
+    if not RXTX then
+      flags = subtree:add("Enabled Modes")     
+        flags:add(p_mmdvm.fields.f_p25en, buf(3,1))     
+        flags:add(p_mmdvm.fields.f_ysfen, buf(3,1))     
+        flags:add(p_mmdvm.fields.f_dmren, buf(3,1))     
+        flags:add(p_mmdvm.fields.f_dstaren, buf(3,1))         
+    
+      subtree:add(buf(4,1), "Modem State: " .. MODE[buf(4,1):uint()])     
+      if (buf(5,1):uint() == 1) then
+        subtree:add(buf(5,1), "Radio Mode            : Transmitting")
+      else
+        subtree:add(buf(5,1), "Radio Mode            : Receiving")
+      end
+      
+      subtree:add(buf(6,1), "DStar Buffer Size     : " .. buf(6,1):uint())
+      subtree:add(buf(7,1), "DMR Slot 1 Buffer Size: " .. buf(7,1):uint())
+      subtree:add(buf(8,1), "DMR Slot 2 Buffer Size: " .. buf(8,1):uint())
+      subtree:add(buf(9,1), "YSF Buffer Size       : " .. buf(9,1):uint())          
+    end  
+
   elseif Command == 0x02 then
     pinfo.cols.info:append("Config ")
-    flags = subtree:add(f_flags, buf(3,2))     
-    flags:add(f_duplex, buf(3,2))     
-    flags:add(f_ysfLoDev, buf(3,2))     
-    flags:add(f_ptti, buf(3,2))     
-    flags:add(f_txi, buf(3,2))     
-    flags:add(f_rxi, buf(3,2))        
-    flags:add(f_p25en, buf(3,2))     
-    flags:add(f_ysfen, buf(3,2))     
-    flags:add(f_dmren, buf(3,2))     
-    flags:add(f_dstaren, buf(3,2))     
+    flags = subtree:add(p_mmdvm.fields.f_flags, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_duplex, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_ysfLoDev, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_ptti, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_txi, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_rxi, buf(3,2))        
+      flags:add(p_mmdvm.fields.f_p25en, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_ysfen, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_dmren, buf(3,2))     
+      flags:add(p_mmdvm.fields.f_dstaren, buf(3,2))     
+    subtree:add(buf(5,1), "TX Delay: " .. buf(5,1):uint() * 10 .. " ms")     
+    subtree:add(buf(6,1), "Init Mode: " .. buf(6,1))     
+    subtree:add(buf(7,1), "RX Level: " .. buf(7,1):uint() * 100 / 255)     
+    subtree:add(buf(8,1), "CDID Level: " .. buf(8,1):uint() * 100 / 255)     
+    subtree:add(buf(9,1), "DMR Color Code: " .. buf(9,1))     
+    subtree:add(buf(10,1), "DMR Delay: " .. buf(10,1))     
+    subtree:add(buf(11,1), "reserved: " .. buf(11,1))     
+    subtree:add(buf(12,1), "DStar TX Level: " .. buf(12,1):uint() * 100 / 255 )     
+    subtree:add(buf(13,1), "DMR TX Level: " .. buf(13,1):uint() * 100 / 255 )     
+    subtree:add(buf(14,1), "YSF TX Level: " .. buf(14,1):uint() * 100 / 255 )     
+    subtree:add(buf(15,1), "P25 TX Level: " .. buf(15,1):uint() * 100 / 255 )     
     
   elseif Command == 0x03 then
     pinfo.cols.info:append("Mode ")
+    subtree:add(buf(3,1), "Mode: " .. MODE[buf(3,1):uint()])     
+    pinfo.cols.info:append(MODE[buf(3,1):uint()])    
   elseif Command == 0x04 then
     pinfo.cols.info:append("Frequency ")
-    subtree:add_le(f_rxf, buf(4,4))         
-    subtree:add_le(f_txf, buf(8,4))         
+    subtree:add_le(p_mmdvm.fields.f_rxf, buf(4,4))         
+    subtree:add_le(p_mmdvm.fields.f_txf, buf(8,4))         
   end
   
   -- DMR
   if Command >= 0x18 and Command <= 0x1E then
-    subtree:add(f_Slot, buf(3,1))
-    subtree:add(f_dsync, buf(3,1))
-    subtree:add(f_async, buf(3,1))
+    subtree:add(p_mmdvm.fields.f_Slot, buf(3,1))
+    subtree:add(p_mmdvm.fields.f_dsync, buf(3,1))
+    subtree:add(p_mmdvm.fields.f_async, buf(3,1))
   
     if not RXTX then
       -- Check for DataSync and Decode low nibble
       if dsync_Field().value == 1 then
-        subtree:add(f_dtype, buf(3,1))
+        subtree:add(p_mmdvm.fields.f_dtype, buf(3,1))
         local dt = dtype_Field().value
         pinfo.cols.info:append("Data SYNC " .. tostring( DATATYPE[dt] ))
       elseif async_Field().value == 1 then
         pinfo.cols.info:append("Voice SYNC ")
       else
-        subtree:add(f_seq, buf(3,1))
+        subtree:add(p_mmdvm.fields.f_seq, buf(3,1))
         pinfo.cols.info:append("Seq " .. tostring(seq_Field().value) .. " ")  
       end
     end  
